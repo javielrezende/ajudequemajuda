d3b4a848ff9a267983d400d2e3f1cd29b3bf9f92
Roger Rezende <javielrezende@gmail.com>
2018-04-29 16:47:04 -0300

    Retiada de seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(EnderecosTableSeeder::class);
-        $this->call(CampanhasTableSeeder::class);
-        $this->call(VisitantesTableSeeder::class);
+        //
     }
 }
