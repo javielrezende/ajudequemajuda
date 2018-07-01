d1e5bbc7c46e7c76a40ad6a6fdc4601e6a7313fd
Roger Rezende <javielrezende@gmail.com>
2018-07-01 20:21:10 -0300

    Front

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,55 +1,46 @@
 @extends('basico')
 
 @section('content')
-        <div class="row centro">
-            <form method="POST" action="{{ route('login') }}">
-                {{ csrf_field() }}
+    <div class="row centro">
+        <form method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
 
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-md-4 control-label">E-Mail</label>
-                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required
-                           autofocus>
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email">E-Mail</label>
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required
+                       autofocus>
 
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                    @endif
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password">Senha</label>
+                <input id="password" type="password" class="form-control" name="password" required>
+
+                @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Lembrar-me
+                    </label>
                 </div>
+            </div>
 
-
-                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-md-4 control-label">Senha</label>
-
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control" name="password" required>
-
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group">
-
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                        </label>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">
-                        Entrar
-                    </button>
-
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        Esqueceu sua senha?
-                    </a>
-                </div>
-            </form>
-        </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Entrar</button>
+                <a class="btn btn-link" href="{{ route('password.request') }}">Esqueceu sua senha?</a>
+            </div>
+        </form>
+    </div>
 @endsection
