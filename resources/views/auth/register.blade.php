e92b70e8873d0a3a0af05668f154dd2e3d3f5768
Roger Rezende <javielrezende@gmail.com>
2018-07-01 23:24:14 -0300

    design

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,192 +1,118 @@
-@extends('layouts.app')
+@extends('basico')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Cadastro</div>
+    <div class="row cadastro">
+        <form method="POST" action="{{ route('register') }}">
+            {{ csrf_field() }}
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
+            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                <label for="name">Nome</label>
+                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required
+                       autofocus>
+                @if ($errors->has('name'))
+                    <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                @endif
+            </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email">E-mail</label>
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                @if ($errors->has('email'))
+                    <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                @endif
+            </div>
 
+            <div class="form-group">
+                <label for="cpf">CPF</label>
+                <input id="cpf" type="text" class="form-control" name="cpf" value="{{ old('cpf') }} ">
+            </div>
 
+            <div class="form-group">
+                <label for="cnpj">CNPJ</label>
+                <input id="cpf" type="text" class="form-control" name="cnpj" value="{{ old('cnpj') }}">
+            </div>
 
+            <div class="form-group">
+                <label for="fone">Fone</label>
+                <input id="fone" type="text" class="form-control" name="fone" value="{{ old('fone') }}">
+            </div>
 
+            <div class="form-group">
+                <label for="rua">Rua</label>
+                <input id="rua" type="text" class="form-control" name="rua" value="{{ old('rua') }}" required>
+            </div>
 
-                        <div class="form-group">
-                            <label for="cpf" class="col-md-4 control-label">CPF</label>
+            <div class="form-group">
+                <label for="numero">Número</label>
+                <input id="numero" type="text" class="form-control" name="numero" value="{{ old('numero') }}" required>
+            </div>
 
-                            <div class="col-md-6">
-                                <input id="cpf" type="text" class="form-control" name="cpf" value="{{ old('cpf') }} ">
-                            </div>
-                        </div>
+            <div class="form-group">
+                <label for="complemento">Complemento</label>
+                <input id="complemento" type="text" class="form-control" name="complemento"
+                       value="{{ old('complemento') }}">
+            </div>
 
-                        <div class="form-group">
-                            <label for="cnpj" class="col-md-4 control-label">CNPJ</label>
+            <div class="form-group">
+                <label for="cidade">Cidade</label>
+                <input id="cidade" type="text" class="form-control" name="cidade" value="{{ old('cidade') }}" required>
+            </div>
 
-                            <div class="col-md-6">
-                                <input id="cpf" type="text" class="form-control" name="cnpj" value="{{ old('cnpj') }}">
-                            </div>
-                        </div>
+            <div class="form-group">
+                <label for="bairro">Bairro</label>
+                <input id="bairro" type="text" class="form-control" name="bairro" value="{{ old('bairro') }}" required>
+            </div>
 
+            <div class="form-group">
+                <label for="cep">Cep</label>
+                <input id="cep" type="text" class="form-control" name="cep" value="{{ old('cep') }}" required>
+            </div>
 
-                        <div class="form-group">
-                            <label for="fone" class="col-md-4 control-label">Fone</label>
+            <div class="form-group">
+                <label for="estado">Estado</label>
+                <input id="estado" type="text" class="form-control" name="estado" value="{{ old('estado') }}" required>
+            </div>
 
-                            <div class="col-md-6">
-                                <input id="fone" type="text" class="form-control" name="fone" value="{{ old('fone') }}">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="rua" class="col-md-4 control-label">Rua</label>
-
-                            <div class="col-md-6">
-                                <input id="rua" type="text" class="form-control" name="rua" value="{{ old('rua') }}" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="numero" class="col-md-4 control-label">Número</label>
-
-                            <div class="col-md-6">
-                                <input id="numero" type="text" class="form-control" name="numero" value="{{ old('numero') }}" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="complemento" class="col-md-4 control-label">Complemento</label>
-
-                            <div class="col-md-6">
-                                <input id="complemento" type="text" class="form-control" name="complemento" value="{{ old('complemento') }}">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="cidade" class="col-md-4 control-label">Cidade</label>
-
-                            <div class="col-md-6">
-                                <input id="cidade" type="text" class="form-control" name="cidade" value="{{ old('cidade') }}" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="bairro" class="col-md-4 control-label">Bairro</label>
-
-                            <div class="col-md-6">
-                                <input id="bairro" type="text" class="form-control" name="bairro" value="{{ old('bairro') }}" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="cep" class="col-md-4 control-label">Cep</label>
-
-                            <div class="col-md-6">
-                                <input id="cep" type="text" class="form-control" name="cep" value="{{ old('cep') }}" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="estado" class="col-md-4 control-label">Estado</label>
-
-                            <div class="col-md-6">
-                                <input id="estado" type="text" class="form-control" name="estado" value="{{ old('estado') }}" required>
-                            </div>
-                        </div>
-
-
-
-
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password">Password</label>
+                <input id="password" type="password" class="form-control" name="password" required>
+                @if ($errors->has('password'))
+                    <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-
-
-
-
-                        <div class="form-group">
-                            <label for="solicitacao_entidade" class="col-md-4 control-label">Solicitar Cadastro como Entidade</label>
-                            <div class="col-md-6">
-                                <input id="solicitacao_entidade" type="checkbox" class="form-control" name="solicitacao_entidade" value={{1}}>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="descricao_entidade" class="col-md-4 control-label">Fale um pouco sobre sua entidade</label>
-                            <div class="col-md-6">
-                                <textarea id="descricao_entidade" class="form-control" name="descricao_entidade"></textarea>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="mensagem" class="col-md-4 control-label">Explique o motivo de seu pedido</label>
-                            <div class="col-md-6">
-                                <textarea id="mensagem" class="form-control" name="mensagem"></textarea>
-                            </div>
-                        </div>
-
-
-
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Cadastrar
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @endif
             </div>
-        </div>
+
+            <div class="form-group">
+                <label for="password-confirm">Confirm Password</label>
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+            </div>
+
+            <div class="form-group">
+                <label for="solicitacao_entidade">Solicitar Cadastro como Entidade</label>
+                <input id="solicitacao_entidade" type="checkbox" class="form-control" name="solicitacao_entidade"
+                       value={{1}}>
+            </div>
+
+            <div class="form-group">
+                <label for="descricao_entidade">Fale um pouco sobre sua entidade</label>
+                <textarea id="descricao_entidade" class="form-control" name="descricao_entidade"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="mensagem">Explique o motivo de seu pedido</label>
+                <textarea id="mensagem" class="form-control" name="mensagem"></textarea>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">
+                    Cadastrar
+                </button>
+            </div>
+        </form>
     </div>
-</div>
 @endsection
