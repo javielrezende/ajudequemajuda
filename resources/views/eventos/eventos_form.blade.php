3a8eacb7d10787b16d6f41dd3acd5657df4282c7
Roger Rezende <javielrezende@gmail.com>
2018-07-05 13:45:09 -0300

    campos obrigatorios em eventos

--- resources/views/eventos/eventos_form.blade.php
+++ resources/views/eventos/eventos_form.blade.php
@@ -57,19 +57,19 @@
                             </div>
 
                             <div class="form-group col-2">
-                                <label for="cep">CEP</label>
+                                <label for="cep">CEP <span class="obr">*</span></label>
                                 <input type="text" class="form-control" id="cep" name="cep"
                                        value="{{$registro->enderecos->cep or old('cep')}}" required>
                             </div>
 
                             <div class="form-group">
-                                <label for="rua">Rua</label>
+                                <label for="rua">Rua <span class="obr">*</span></label>
                                 <input type="text" class="form-control" id="rua" name="rua"
                                        value="{{$registro->enderecos->rua or old('rua')}}" required>
                             </div>
 
                             <div class="form-group col-1">
-                                <label for="numero">Nº</label>
+                                <label for="numero">Nº <span class="obr">*</span></label>
                                 <input type="text" class="form-control" id="numero" name="numero"
                                        value="{{$registro->enderecos->numero or old('numero')}}" required>
                             </div>
@@ -77,23 +77,23 @@
                             <div class="form-group">
                                 <label for="complemento">Complemento</label>
                                 <input type="text" class="form-control" id="complemento" name="complemento"
-                                       value="{{$registro->enderecos->complemento or old('complemento')}}" required>
+                                       value="{{$registro->enderecos->complemento or old('complemento')}}">
                             </div>
 
                             <div class="form-group col-2">
-                                <label for="bairro">Bairro</label>
+                                <label for="bairro">Bairro <span class="obr">*</span></label>
                                 <input type="text" class="form-control" id="bairro" name="bairro"
                                        value="{{$registro->enderecos->bairro or old('bairro')}}" required>
                             </div>
 
                             <div class="form-group col-2">
-                                <label for="cidade">Cidade</label>
+                                <label for="cidade">Cidade <span class="obr">*</span></label>
                                 <input type="text" class="form-control" id="cidade" name="cidade"
                                        value="{{$registro->enderecos->cidade or old('cidade')}}" required>
                             </div>
 
                             <div class="form-group">
-                                <label for="estado">Estado</label>
+                                <label for="estado">Estado <span class="obr">*</span></label>
                                 <input type="text" class="form-control" id="estado" name="estado"
                                        value="{{$registro->enderecos->estado or old('estado')}}" required>
                             </div>
