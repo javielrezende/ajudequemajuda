0b904cbc63f1d95e7678364c97a9e51b6d541936
Roger Rezende <javielrezende@gmail.com>
2018-08-13 18:10:41 -0300

    add imagens e coninuacao de programacao de front pag principal

---
+++ resources/views/rodapesite.blade.php
@@ -0,0 +1,11 @@
+<div class="row rodape">
+    <div class="direitos col-6">
+        <p>2018. AjudeQuemAjuda - Todos direitos reservados</p>
+    </div>
+    <div class="faq col-6">
+        <a href="#"><p>SOBRE</p></a>
+        <a href=""><p class="faqcentro">CONTATO</p></a>
+        <a href=""><p>FAQ</p></a>
+
+    </div>
+</div>
--- resources/views/welcomesite.blade.php
+++ resources/views/welcomesite.blade.php
@@ -1,3 +1,4 @@
+@extends('rodapesite')
 @extends('basicosite')
 @section('content')
     <div class="row welcome">
@@ -11,11 +12,11 @@
         </div>
     </div>
 
-    <div class="row imginstrucoes">
+    <div class="row instrucoes">
         <p class="row col-12 ">Como funciona?</p>
         <div class="row circulos">
             <img class="imagemcirculo" src="{{ asset('imagens/circulo.png') }}"
-                 alt="circulos">
+                 alt="circulos"  >
             <img class="imagemcirculo" src="{{ asset('imagens/circulo.png') }}"
                  alt="circulos">
             <img class="imagemcirculo" src="{{ asset('imagens/circulo.png') }}"
@@ -23,6 +24,31 @@
             <img class="imagemcirculo" src="{{ asset('imagens/circulo.png') }}"
                  alt="circulos">
         </div>
+        <div class="row imginstrucoes">
+            <img class="imageminstrucao" src="{{ asset('imagens/1.png') }}"
+                 alt="instrucao">
+            <img class="imageminstrucao" src="{{ asset('imagens/2.png') }}"
+                 alt="instrucao">
+            <img class="imageminstrucao" src="{{ asset('imagens/3.png') }}"
+                 alt="instrucao">
+            <img class="imageminstrucao" src="{{ asset('imagens/4.png') }}"
+                 alt="instrucao">
+        </div>
     </div>
+
+    <div class="row sitedestaques">
+        <p class="row col-12 titulodestaques">Campanhas em destaque</p>
+        <div class="row imgdestaques">
+            <div class="row col-6 divdestaques">
+                <div class="imagemdestaque"></div>
+                <h4 class="nomecampanhadestaque">Campanha 1</h4>
+                <p class="datacampanhadestaque"> 22/03/2018 </p>
+                <p class="descricaocampanhadestaque"> Lorem ipsum dolor sit amet, con... </p>
+                <p class="saibamaiscampanhadestaque"> Saiba mais</p>
+            </div>
+        </div>
+    </div>
+
+
 
 @endsection
