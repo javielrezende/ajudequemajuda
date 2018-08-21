b26914831bff9e7a052f16336e158160f88c9397
Roger Rezende <javielrezende@gmail.com>
2018-08-21 19:50:49 -0300

    imagem evento

--- resources/views/rodapesite.blade.php
+++ resources/views/rodapesite.blade.php
@@ -1,6 +1,7 @@
 <div class="row rodape">
     <hr>
     <div class="direitos col-6">
+        <a href="{{ url('/') }}"><img class="logorodape" alt="logo cinza" src="{{ asset('imagens/logo_cinza.png') }}"></a>
         <p>2018. AjudeQuemAjuda - Todos direitos reservados</p>
     </div>
     <div class="faq col-6">
