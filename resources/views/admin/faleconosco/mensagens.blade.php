9bca870e65907f2553ad879e1e66a0b336ccd078
Roger Rezende <javielrezende@gmail.com>
2018-09-19 18:43:06 -0300

    Mostrando botoes de forma diferente quanda a mensagem ja tiver sido vista

--- resources/views/admin/faleconosco/mensagens.blade.php
+++ resources/views/admin/faleconosco/mensagens.blade.php
@@ -3,52 +3,58 @@
 @section('content')
     <div class="width">
         {{--<div class="row">--}}
-            <div>
-                <h2> Mensagens do Fale Conosco </h2>
-            </div>
-            <div>
-                &nbsp;&nbsp;&nbsp;<a href="{{url('/admin')}}" class="btn btn-outline-primary btn-sm">Voltar</a>
-            </div>
+        <div>
+            <h2> Mensagens do Fale Conosco </h2>
+        </div>
+        <div>
+            &nbsp;&nbsp;&nbsp;<a href="{{url('/admin')}}" class="btn btn-outline-primary btn-sm">Voltar</a>
+        </div>
 
-            <div class='row'>
+        <div class='row'>
 
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-                <table class="table table-hover">
-                    <thead>
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Cidade</th>
+                    <th>Estado</th>
+                    <th>Fone</th>
+                    <th>Ações</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($mensagens as $mensagem)
                     <tr>
-                        <th>Id</th>
-                        <th>Nome</th>
-                        <th>E-mail</th>
-                        <th>Cidade</th>
-                        <th>Estado</th>
-                        <th>Fone</th>
-                        <th>Ações</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($mensagens as $mensagem)
-                        <tr>
-                            <td>{{$mensagem->id}}</td>
-                            <td>{{$mensagem->nome}}</td>
-                            <td>{{$mensagem->email}}</td>
-                            <td>{{$mensagem->cidade}}</td>
-                            <td>{{$mensagem->estado}}</td>
-                            <td>{{$mensagem->fone}}</td>
-                            <td class="row">
+                        <td>{{$mensagem->id}}</td>
+                        <td>{{$mensagem->nome}}</td>
+                        <td>{{$mensagem->email}}</td>
+                        <td>{{$mensagem->cidade}}</td>
+                        <td>{{$mensagem->estado}}</td>
+                        <td>{{$mensagem->fone}}</td>
+                        <td class="row">
+                            @if($mensagem->status == 0)
                                 <div><a href="{{route('faleconoscoadmin.edit', $mensagem->id)}}"
                                         class="btn btn-outline-warning btn-sm m-2">Ver mensagem</a>
                                 </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
+                            @else
+                                <div><a href="{{route('faleconoscoadmin.edit', $mensagem->id)}}"
+                                        class="btn btn-outline-light btn-sm m-2">Ver mensagem</a>
+                                </div>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
         {{--</div>--}}
     </div>
 @endsection
