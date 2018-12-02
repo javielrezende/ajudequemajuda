19960c03b25c0e63a186583e5aa91a0fa4f83c53
Roger Rezende <javielrezende@gmail.com>
2018-12-02 18:10:30 -0200

    Alterando linguagens de aviso para portugues

--- resources/views/site/cadastro/loginsite.blade.php
+++ resources/views/site/cadastro/loginsite.blade.php
@@ -8,6 +8,18 @@
 
         <p class="row col-md-12 titulosPrincipais">Login</p>
 
+        <div class="col-md-12">
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        </div>
+
         <form class="row formcadastro" method="post" action="{{route('login')}}">
             {{ csrf_field() }}
 
